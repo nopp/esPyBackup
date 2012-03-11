4c6c2c53c322b46c90b84433afffc29eab46c046
Carlos Augusto Malucelli <carlos@carlosmalucelli.com>
2012-03-11 01:58:02 -0300

    update

--- esPyBackup.py
+++ esPyBackup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 #
-# esPyBackup - Simple backup of elasticsearch
+# esPyBackup - Simple backup for elasticsearch
 # Author: Carlos Augusto Malucelli
 # www.carlosmalucelli.com
 # https://github.com/malucelli/esPyBackup
