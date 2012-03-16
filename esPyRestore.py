58c36c6d191c9d7f923e1e12367c19030152f62e
Carlos Augusto Malucelli <carlos@carlosmalucelli.com>
2012-03-16 04:19:03 -0300

    update

--- esPyRestore.py
+++ esPyRestore.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
-# coding: utf-8
-#
 # esPyBackup - Simple backup for elasticsearch
-# Author: Carlos Augusto Malucelli
-# www.carlosmalucelli.com
-# https://github.com/malucelli/esPyBackup
-#
 
 # esPyBackup Moduels
 from esLib import EsRestore
